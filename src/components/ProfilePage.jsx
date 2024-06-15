c6ba4859a6d0bbb2e94ba63ecd99a0a960430e6c
Pushkar Sareen <sareenpushkar46@gmail.com>
2024-06-16 01:41:44 +0200

    login

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,39 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { getUserProfile } from '../services/api';
+import React from 'react';
+import profileImage from "../components/assets/image/profile.png"
 
 const ProfilePage = () => {
-  const [profile, setProfile] = useState(null);
-
-  useEffect(() => {
-    async function fetchProfile() {
-      const userProfile = await getUserProfile();
-      setProfile(userProfile);
-    }
-
-    fetchProfile();
-  }, []);
-
   return (
     <div>
-      <h2>Profile</h2>
-      {profile && (
-        <div>
-          <img src={profile.avatar} alt={profile.name} />
-          <h3>{profile.name}</h3>
-          <p>{profile.bio}</p>
-          <p>LinkedIn: <a href={profile.linkedin} target="_blank" rel="noopener noreferrer">{profile.linkedin}</a></p>
-          <p>Smile Tokens: {profile.smileTokens}</p>
-          <p>Rocket Tokens: {profile.rocketTokens}</p>
-          <p>Last Donations:</p>
-          <ul>
-            {profile.lastDonations.map((donation, index) => (
-              <li key={index}>{donation}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <img src={profileImage} alt='profile' />
     </div>
-  );
-};
+  )
+}
 
-export default ProfilePage;
+export default ProfilePage
---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import signImage from "../components/assets/image/signup.png"
+
+const Register = () => {
+ 
+  return (
+    <div >
+      <img src={signImage} alt='signup' />
+    </div>
+  )
+}
+
+export default Register;
